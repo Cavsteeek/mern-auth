91979f892a1a08be2fb55f1b5a88378ee228bf97
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2025-10-18 18:13:47 +0100

    feat: implement routing and add main pages (Home, Login, ResetPassword, EmailVerify) with Header and Navbar components

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import Home from './pages/Home.jsx'
+import Login from './pages/Login.jsx'
+import ResetPassword from './pages/ResetPassword.jsx'
+import EmailVerify from './pages/EmailVerify.jsx'
 
 const App = () => {
   return (
     <div>
-      Hello
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+        <Route path="/email-verify" element={<EmailVerify />} />
+        <Route path="/reset-password" element={<ResetPassword />} />
+      </Routes>
     </div>
   )
 }
 
-export default App
-
+export default App;
---
+++ client/src/pages/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/Navbar.jsx'
+import Header from '../components/Header.jsx'
+
+
+const Home = () => {
+  return (
+    <div>
+      <Header/>
+      <Navbar />
+    </div>
+  )
+}
+
+export default Home
