d68aec02b8b6ffca3200b66a96d04778c632ef92
Uzochukwu Ikemdinachi <uzochukwuikemdinachi@gmail.com>
2026-01-13 14:55:45 +0100

    set state for the forms

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -8,33 +8,48 @@ const Login = () => {
 
   return (
     <div className='flex items-center justify-center min-h-screen px-6 sm:px-0 bg-gradient-to-br from-blue-200 to-purple-400'>
-        <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer'/>
-        <div className='bg-slate-900 p-10 rounded-xl shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
-          
-          <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? "Create account" : "Login"}</h2>
-          
-          <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? "Create your account" : "Login to your account"}</p>
+      <img src={assets.logo} alt="" className='absolute left-5 sm:left-20 top-5 w-28 sm:w-32 cursor-pointer' />
+      <div className='bg-slate-900 p-10 rounded-xl shadow-lg w-full sm:w-96 text-indigo-300 text-sm'>
 
-          <form action="">
+        <h2 className='text-3xl font-semibold text-white text-center mb-3'>{state === 'Sign Up' ? "Create account" : "Login"}</h2>
 
-            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
-              <img src={assets.person_icon} alt="" />
-              <input className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
-            </div>
+        <p className='text-center text-sm mb-6'>{state === 'Sign Up' ? "Create your account" : "Login to your account"}</p>
 
-            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
-              <img src={assets.person_icon} alt="" />
-              <input className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
-            </div>
+        <form>
+          {state === 'Sign Up' && (<div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            <img src={assets.person_icon} alt="" />
+            <input className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
+          </div>
+          )}
 
-            <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
-              <img src={assets.person_icon} alt="" />
-              <input className='bg-transparent outline-none text-white' type="text" placeholder='Full Name' required />
-            </div>
-          </form>
-        </div>
 
-        
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            <img src={assets.mail_icon} alt="" />
+            <input className='bg-transparent outline-none text-white' type="email" placeholder='Email' required />
+          </div>
+
+          <div className='mb-4 flex items-center gap-3 w-full px-5 py-2.5 rounded-full bg-[#333A5C]'>
+            <img src={assets.lock_icon} alt="" />
+            <input className='bg-transparent outline-none text-white' type="password" placeholder='Password' required />
+          </div>
+
+          <p className='mb-4 text-indigo-500 cursor-pointer'>Forgot Password?</p>
+
+          <button className='w-full bg-gradient-to-r from-indigo-500 to-purple-900 py-2.5 rounded-full text-white font-semibold hover:from-indigo-600 hover:to-purple-600 transition-all mb-4'>{state}</button>
+        </form>
+
+        {state === 'Sign Up' ?
+          (<p className='text-gray-400 text-center text-xs mt-4'>Already have an account?{' '}
+            <span onClick={() => setState('Login')} className='text-blue-400 cursor-pointer underline'>Login here</span>
+          </p>) :
+          (<p className='text-gray-400 text-center text-xs mt-4'>Don't have an account?{' '}
+            <span onClick={() => setState('Sign Up')} className='text-blue-400 cursor-pointer underline'>Sign up</span>
+          </p>)}
+
+
+
+
+      </div>
     </div>
   )
 }
